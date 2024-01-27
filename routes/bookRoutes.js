1522b583b43534cf4ab2134b2e9474db86b18309
magupta1 <mg328790@gmail.com>
2024-01-27 21:45:56 +0530

    update and delete api

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -20,7 +20,30 @@ bookRouter.get("/", async (req, res) => {
     const books = await BookModel.find();
     res.status(200).send(books);
   } catch (error) {
-    res.status(400).send({error: error});
+    res.status(400).send({ error: error });
+  }
+});
+
+// update books
+bookRouter.patch("update/:bookid", async (req, res) => {
+  const { bookid } = req.params;
+  try {
+    await BookModel.findByIdAndUpdate({ _id: bookid }, req.body);
+    res.status(200).send({ msg: "Book has been updated" });
+  } catch (error) {
+    res.status(400).send({ error: error });
+  }
+});
+
+// delete books
+bookRouter.delete("delete/:bookid", async (req, res) => {
+  const { bookid } = req.params;
+  try {
+    console.log(req.params)
+    await BookModel.findByIdAndDelete({ _id: bookid }, req.body);
+    res.status(200).send({ msg: "Book has been deleted" });
+  } catch (error) {
+    res.status(400).send({ error: error });
   }
 });
 
